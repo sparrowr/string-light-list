fdbe3d6aa01b8f4463476e989def232ab182840e
sparrowr <sparrow@srubin.org>
2018-04-23 14:26:00 -0400

    fixing typo in config file
    hopefully connections will work in deployment once this is done

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://cryptic-refuge-45274.herokuapp.com/',
+  production: 'https://cryptic-refuge-45274.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
